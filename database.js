338526014d33fe3a4a0d8be5325adfb43a14afa9
ThiagoAbbudCoutinho <t.abbud@gmail.com>
2022-05-16 22:26:35 +0000

    process env port

--- database.js
+++ database.js
@@ -1,9 +1,15 @@
-//import
-require('dotenv').config();
-const mongoose = require('mongoose');
-//connect to db via DB's url
-const ATLAS_URI = process.env.ATLAS_URI;
-mongoose.connect(ATLAS_URI,{useNewUrlParser:true})
-    .then(() => console.log('db connected hi mongon'))
-    .catch(e => console.log(e));
-//All other calls to DB are made on serve
+require('dotenv').config()
+const mongoose  = require('mongoose');
+const uri = process.env.ATLAS_URI;
+//const client = new MongoClient(uri);
+
+(async () => {
+  try {
+    await mongoose.connect;
+    console.log(
+      `Role of database - Host: `
+    );
+  } catch (e) {
+    console.log('Error: ', e.message);
+  }
+})();
